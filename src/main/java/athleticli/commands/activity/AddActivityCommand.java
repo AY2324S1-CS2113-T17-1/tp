b6b92040342ba95378e7a324ff3e28998dfb633f
AlWo223 <alexander.wolters@tum.de>
2023-10-12 16:06:46 +0800

    Implement adding of specific activities swim, cycle and run

--- src/main/java/athleticli/commands/activity/AddActivityCommand.java
+++ src/main/java/athleticli/commands/activity/AddActivityCommand.java
@@ -1,7 +1,9 @@
 package athleticli.commands.activity;
 
+import athleticli.data.activity.Activity;
 import athleticli.data.activity.ActivityList;
 import athleticli.data.activity.Run;
+import athleticli.data.activity.Swim;
 import athleticli.exceptions.EmptyArgumentException;
 import athleticli.exceptions.UnknownCommandException;
 import athleticli.ui.Ui;
@@ -9,14 +11,16 @@ import athleticli.ui.Ui;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Executes the add activity commands provided by the user.
  */
 public class AddActivityCommand {
 
-    private String command;
+    private final String command;
     private String argument;
+    private ActivityType activityType;
 
     private ActivityList activityList;
     private Ui ui;
@@ -31,7 +35,7 @@ public class AddActivityCommand {
      * @param argument Arguments required for the specific command.
      * */
     public AddActivityCommand(String command, String argument, ActivityList activityList, Ui ui) {
-        ActivityType activityType = ActivityType.valueOf(command.toUpperCase());
+        this.activityType = ActivityType.valueOf(command.toUpperCase());
         this.command = command;
         this.argument = argument;
         this.activityList = activityList;
@@ -45,14 +49,23 @@ public class AddActivityCommand {
      */
     public ActivityList execute() {
         try {
-            ActivityType activityType = getActivityType(command);
-            switch(activityType) {
+            if (this.argument == null || this.argument.isEmpty()) {
+                throw new EmptyArgumentException();
+            }
+
+            this.activityType = getActivityType(command);
+
+            switch(this.activityType) {
             case ACTIVITY:
                 activityList = addActivity();
                 break;
             case CYCLE:
             case RUN:
+                activityList = addRunCycle();
+                break;
             case SWIM:
+                activityList = addSwim();
+                break;
             default:
                 throw new UnknownCommandException();
             }
@@ -64,34 +77,49 @@ public class AddActivityCommand {
 
 
     /**
+     * Adds a running activity to the activity list.
+     * @return ActivityList List of activities with the added run.
+     * @throws UnknownCommandException If the command is not valid.
+     * @throws EmptyArgumentException If the provided argument is empty.
+     * */
+    public ActivityList addRunCycle() throws UnknownCommandException, EmptyArgumentException {
+        try {
+            ArrayList<String> separators = new ArrayList<String>(Arrays.asList(" duration/", " distance/", " datetime/", " elevation/"));
+            ArrayList<String> params = extractParameters(separators);
+
+            String caption = params.get(0);
+            int duration = Integer.parseInt(params.get(1));
+            int distance = Integer.parseInt(params.get(2));
+            LocalDateTime datetime = LocalDateTime.parse(params.get(3).replace(" ", "T"));
+            int elevationGain = Integer.parseInt(params.get(4));
+
+            this.activityList.add(new Run(caption, duration, distance, datetime, elevationGain));
+
+            return this.activityList;
+        } catch (ArrayIndexOutOfBoundsException | NumberFormatException | NullPointerException |
+                 DateTimeParseException e) {
+            throw new UnknownCommandException();
+        }
+    }
+
+    /**
      * Adds a general activity to the activity list.
      * @return ActivityList List of activities with the added activity.
      * @throws UnknownCommandException If the command is not valid.
      * @throws EmptyArgumentException If the provided argument is empty.
      * */
     public ActivityList addActivity() throws UnknownCommandException, EmptyArgumentException {
-        if (this.argument == null || this.argument.isEmpty()) {
-            throw new EmptyArgumentException();
-        }
         try {
             ArrayList<String> separators = new ArrayList<String>();
-            separators.add(" duration/");
-            separators.add(" distance/");
-            separators.add(" datetime/");
-            separators.add(" elevation/");
+            separators.addAll(Arrays.asList(" duration/", " distance/", " datetime/"));
+            ArrayList<String> params = extractParameters(separators);
 
-            ArrayList<String> params = new ArrayList<String>();
-            for (String separator : separators) {
-                params.add(this.argument.split(separator)[0]);
-                this.argument = this.argument.split(separator)[1];
-            }
-
-            int elevationGain = Integer.parseInt(this.argument);
             String caption = params.get(0);
             int duration = Integer.parseInt(params.get(1));
             int distance = Integer.parseInt(params.get(2));
             LocalDateTime datetime = LocalDateTime.parse(params.get(3).replace(" ", "T"));
-            this.activityList.add(new Run(caption, duration, distance, datetime, elevationGain));
+
+            this.activityList.add(new Activity(caption, duration, distance, datetime));
 
             return this.activityList;
         } catch (ArrayIndexOutOfBoundsException | NumberFormatException | NullPointerException |
@@ -110,6 +138,46 @@ public class AddActivityCommand {
         try {
             return ActivityType.valueOf(command.toUpperCase());
         } catch (IllegalArgumentException e) {
+            throw new UnknownCommandException();
+        }
+    }
+
+    /**
+     * Extracts the different parameters from the argument string.
+     * @param separators List of separators, i.e. the specific formatting, used to split the argument string.
+     * @return List of parameters.
+     */
+    private ArrayList<String> extractParameters(ArrayList<String> separators) throws ArrayIndexOutOfBoundsException {
+        ArrayList<String> params = new ArrayList<String>();
+        for (String separator : separators) {
+            params.add(this.argument.split(separator)[0]);
+            this.argument = this.argument.split(separator)[1];
+        }
+        params.add(this.argument);
+        return params;
+    }
+
+    /**
+     * Adds a swimming activity to the activity list.
+     * @return ActivityList List of activities with the added swim.
+     * @throws UnknownCommandException If the command is not valid.
+     * @throws EmptyArgumentException If the provided argument is empty.
+     * */
+    public ActivityList addSwim() throws UnknownCommandException, EmptyArgumentException {
+        try {
+            ArrayList<String> separators = new ArrayList<String>(Arrays.asList(" duration/", " distance/", " datetime/", " style/"));
+            ArrayList<String> params = extractParameters(separators);
+
+            String caption = params.get(0);
+            int duration = Integer.parseInt(params.get(1));
+            int distance = Integer.parseInt(params.get(2));
+            LocalDateTime datetime = LocalDateTime.parse(params.get(3).replace(" ", "T"));
+            Swim.SwimmingStyle style = Swim.SwimmingStyle.valueOf(params.get(4));
+            this.activityList.add(new Swim(caption, duration, distance, datetime, style));
+
+            return this.activityList;
+        } catch (ArrayIndexOutOfBoundsException | NumberFormatException | NullPointerException |
+                 DateTimeParseException e) {
             throw new UnknownCommandException();
         }
     }
