22dddf5c7a3a6aab26c295f9c95e46fff7788447
AlWo223 <alexander.wolters@tum.de>
2023-10-12 23:52:52 +0800

    Implement activity parsing

--- src/main/java/athleticli/ui/Message.java
+++ src/main/java/athleticli/ui/Message.java
@@ -7,4 +7,15 @@ public class Message {
     public static final String PREFIX_EXCEPTION = "OOPS!!! ";
     public static final String MESSAGE_BYE = "Bye. Hope to see you again soon!";
     public static final String[] MESSAGE_HELLO = {"Hello! I'm AthletiCLI!", "What can I do for you?"};
+    public static final String MESSAGE_CAPTION_MISSING = "The caption of an activity cannot be empty!";
+    public static final String MESSAGE_DURATION_MISSING = "Please specify the activity duration using \"duration/\"!";
+    public static final String MESSAGE_DISTANCE_MISSING = "Please specify the activity duration using \"distance/\"!";
+    public static final String MESSAGE_DATETIME_MISSING = "Please specify the activity duration using \"datetime/\"!";
+    public static final String MESSAGE_CAPTION_EMPTY = "The caption of an activity cannot be empty!";
+    public static final String MESSAGE_DURATION_EMPTY = "The duration of an activity cannot be empty!";
+    public static final String MESSAGE_DISTANCE_EMPTY = "The distance of an activity cannot be empty!";
+    public static final String MESSAGE_DATETIME_EMPTY = "The datetime of an activity cannot be empty!";
+    public static final String MESSAGE_DURATION_INVALID = "The duration of an activity must be a positive integer!";
+    public static final String MESSAGE_DISTANCE_INVALID = "The distance of an activity must be a positive integer!";
+    public static final String MESSAGE_DATETIME_INVALID = "The datetime of an activity must be in the format \"yyyy-MM-dd HH:mm\"!";
 }
--- src/main/java/athleticli/ui/Parser.java
+++ src/main/java/athleticli/ui/Parser.java
@@ -2,8 +2,13 @@ package athleticli.ui;
 
 import athleticli.commands.ByeCommand;
 import athleticli.commands.Command;
+import athleticli.data.activity.Activity;
 import athleticli.exceptions.AthletiException;
 import athleticli.exceptions.UnknownCommandException;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
+import java.util.Locale;
 
 /**
  * Defines the basic methods for command parser.
@@ -45,4 +50,62 @@ public class Parser {
             throw e;
         }
     }
+
+    public static Activity parseActivity(String arguments) throws AthletiException {
+        final int durationIndex = arguments.indexOf("duration/");
+        final int distanceIndex = arguments.indexOf("distance/");
+        final int datetimeIndex = arguments.indexOf("datetime/");
+
+        if (durationIndex == -1) {
+            throw new AthletiException(Message.MESSAGE_DURATION_MISSING);
+        }
+        if (distanceIndex == -1) {
+            throw new AthletiException(Message.MESSAGE_DISTANCE_MISSING);
+        }
+        if (datetimeIndex == -1) {
+            throw new AthletiException(Message.MESSAGE_DATETIME_MISSING);
+        }
+
+        final String caption = arguments.substring(0, durationIndex).trim();
+        final String duration = arguments.substring(durationIndex + 9, distanceIndex).trim();
+        final String distance = arguments.substring(distanceIndex + 9, datetimeIndex).trim();
+        final String datetime = arguments.substring(datetimeIndex + 9).trim();
+        if (caption.isEmpty()) {
+            throw new AthletiException(Message.MESSAGE_CAPTION_EMPTY);
+        }
+        if (duration.isEmpty()) {
+            throw new AthletiException(Message.MESSAGE_DURATION_EMPTY);
+        }
+        if (distance.isEmpty()) {
+            throw new AthletiException(Message.MESSAGE_DISTANCE_EMPTY);
+        }
+        if (datetime.isEmpty()) {
+            throw new AthletiException(Message.MESSAGE_DATETIME_EMPTY);
+        }
+        Activity activity = parseActivityArguments(caption, duration, distance, datetime);
+    }
+
+    public static Activity parseActivityArguments(String caption, String duration, String distance, String datetime)
+            throws AthletiException {
+        final int durationParsed;
+        final int distanceParsed;
+        final LocalDateTime datetimeParsed;
+        try {
+            durationParsed = Integer.parseInt(duration);
+        } catch (NumberFormatException e) {
+            throw new AthletiException(Message.MESSAGE_DURATION_INVALID);
+        }
+        try {
+            distanceParsed = Integer.parseInt(distance);
+        } catch (NumberFormatException e) {
+            throw new AthletiException(Message.MESSAGE_DISTANCE_INVALID);
+        }
+        try {
+            datetimeParsed = LocalDateTime.parse(datetime.replace(" ", "T"));
+        } catch (DateTimeParseException e) {
+            throw new AthletiException(Message.MESSAGE_DATETIME_INVALID);
+        }
+        return new Activity(caption, durationParsed, distanceParsed, datetimeParsed);
+    }
+
 }
