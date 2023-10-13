f48538ce055185165435ec50881e37e26220e28b
nihalzp <81457724+nihalzp@users.noreply.github.com>
2023-10-14 00:36:55 +0800

    Implement DietList

--- src/main/java/athleticli/data/diet/DietList.java
+++ src/main/java/athleticli/data/diet/DietList.java
@@ -2,5 +2,28 @@ package athleticli.data.diet;
 
 import java.util.ArrayList;
 
+/**
+ * Represents a list of diets.
+ */
 public class DietList extends ArrayList<Diet> {
+    /**
+     * Constructs a diet list.
+     */
+    public DietList() {
+        super();
+    }
+
+    /**
+     * Returns a string representation of the diet list.
+     *
+     * @return A string representation of the diet list.
+     */
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < size(); i++) {
+            result.append(i + 1).append(". ").append(get(i).toString());
+        }
+        return result.toString();
+    }
 }
