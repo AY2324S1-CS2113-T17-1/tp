020e4dbefe29062a29b5f8e2eb7e86e20daa3f11
nihalzp <81457724+nihalzp@users.noreply.github.com>
2023-10-14 00:14:48 +0800

    Add JavaDoc comments for diet goal

--- src/main/java/athleticli/data/diet/DietGoal.java
+++ src/main/java/athleticli/data/diet/DietGoal.java
@@ -1,12 +1,20 @@
 package athleticli.data.diet;
 
+/**
+ * Represents a diet goal.
+ */
 public class DietGoal {
     private String nutrients;
     private int targetValue;
     private int currentValue;
     private boolean isGoalAchieved;
 
-
+    /**
+     * Constructs a diet goal with no current value.
+     *
+     * @param nutrients   The nutrients of the diet goal.
+     * @param targetValue The target value of the diet goal.
+     */
     public DietGoal(String nutrients, int targetValue) {
         this.nutrients = nutrients;
         this.targetValue = targetValue;
@@ -14,34 +22,65 @@ public class DietGoal {
         isGoalAchieved = false;
     }
 
+    /**
+     * Constructs a diet goal with a current value.
+     *
+     * @param nutrients    The nutrients of the diet goal.
+     * @param targetValue  The target value of the diet goal.
+     * @param currentValue The current value of the diet goal.
+     */
     public DietGoal(String nutrients, int targetValue, int currentValue) {
         this.nutrients = nutrients;
         this.targetValue = targetValue;
         this.currentValue = currentValue;
         isGoalAchieved = currentValue >= targetValue;
     }
-
+    
+    /**
+     * Returns the nutrients of the diet goal.
+     */
     public String getNutrients() {
         return nutrients;
     }
 
+    /**
+     * Sets the nutrients of the diet goal.
+     *
+     * @param nutrients The nutrients of the diet goal.
+     */
     public void setNutrients(String nutrients) {
         this.nutrients = nutrients;
     }
 
+    /**
+     * Returns the target value of the diet goal.
+     */
     public int getTargetValue() {
         return targetValue;
     }
 
+    /**
+     * Sets the target value of the diet goal.
+     *
+     * @param targetValue The target value of the diet goal.
+     */
     public void setTargetValue(int targetValue) {
         this.targetValue = targetValue;
         setIsGoalAchieved(currentValue >= targetValue);
     }
 
+    /**
+     * Returns the current value of the diet goal.
+     */
     public int getCurrentValue() {
         return currentValue;
     }
 
+    /**
+     * Sets the current value of the diet goal.
+     *
+     * @param currentValue The current value of the diet goal.
+     */
     public void setCurrentValue(int currentValue) {
         this.currentValue = currentValue;
         if (!isGoalAchieved && currentValue >= targetValue) {
@@ -51,17 +90,27 @@ public class DietGoal {
         }
     }
 
+    /**
+     * Returns whether the diet goal is achieved.
+     */
     public boolean getIsGoalAchieved() {
         return isGoalAchieved;
     }
 
+    /**
+     * Sets whether the diet goal is achieved.
+     *
+     * @param isGoalAchieved Whether the diet goal is achieved.
+     */
     private void setIsGoalAchieved(boolean isGoalAchieved) {
         this.isGoalAchieved = isGoalAchieved;
     }
 
+    /**
+     * Returns the string representation of the diet goal.
+     */
     @Override
     public String toString() {
-        return nutrients + " intake progress: (" + currentValue
-                + "/" + targetValue + ")\n";
+        return nutrients + " intake progress: (" + currentValue + "/" + targetValue + ")\n";
     }
 }
--- src/main/java/athleticli/data/diet/DietGoalList.java
+++ src/main/java/athleticli/data/diet/DietGoalList.java
@@ -2,11 +2,20 @@ package athleticli.data.diet;
 
 import java.util.ArrayList;
 
+/**
+ * Represents a list of diet goals.
+ */
 public class DietGoalList extends ArrayList<DietGoal> {
+    /**
+     * Constructs a diet goal list.
+     */
     public DietGoalList() {
         super();
     }
-    
+
+    /**
+     * Returns a string representation of the diet goal list.
+     */
     @Override
     public String toString() {
         StringBuilder result = new StringBuilder();
