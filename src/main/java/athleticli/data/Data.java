93d65605f87a62f40c372370935fb104f66a5771
DaDevChia <88506363+DaDevChia@users.noreply.github.com>
2023-10-14 16:09:40 +0800

    Renamed all meal to diet class reference

--- src/main/java/athleticli/data/Data.java
+++ src/main/java/athleticli/data/Data.java
@@ -42,8 +42,8 @@ public class Data {
         return activityGoals;
     }
 
-    public MealList getMeals() {
-        return meals;
+    public DietList getDiets() {
+        return diets;
     }
 
     public DietGoalList getDietGoals() {
