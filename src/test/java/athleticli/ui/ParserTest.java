119b279f86e540ae3cb4b7135023cd123a440bcc
DaDevChia <88506363+DaDevChia@users.noreply.github.com>
2023-10-14 16:18:12 +0800

    Added junit tests for sleep commands in parser

--- src/test/java/athleticli/ui/ParserTest.java
+++ src/test/java/athleticli/ui/ParserTest.java
@@ -9,6 +9,11 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
 import athleticli.commands.ByeCommand;
+import athleticli.commands.sleep.AddSleepCommand;
+import athleticli.commands.sleep.EditSleepCommand;
+import athleticli.commands.sleep.DeleteSleepCommand;
+import athleticli.commands.sleep.ListSleepCommand;
+
 import athleticli.exceptions.AthletiException;
 import athleticli.exceptions.UnknownCommandException;
 
@@ -36,5 +41,29 @@ class ParserTest {
     void parseCommand_byeCommand_expectByeCommand() throws AthletiException {
         final String byeCommand = "bye";
         assertInstanceOf(ByeCommand.class, parseCommand(byeCommand));
+    }
+
+    @Test
+    void parseCommand_addSleepCommand_expectAddSleepCommand() throws AthletiException {
+        final String addSleepCommandString = "add-sleep /start 10:00 PM /end 6:00 AM";
+        assertInstanceOf(AddSleepCommand.class, parseCommand(addSleepCommandString));
+    }
+
+    @Test
+    void parseCommand_editSleepCommand_expectEditSleepCommand() throws AthletiException {
+        final String editSleepCommandString = "edit-sleep 1 /start 10:00 PM /end 6:00 AM";
+        assertInstanceOf(EditSleepCommand.class, parseCommand(editSleepCommandString));
+    }
+
+    @Test
+    void parseCommand_deleteSleepCommand_expectDeleteSleepCommand() throws AthletiException {
+        final String deleteSleepCommandString = "delete-sleep 1";
+        assertInstanceOf(DeleteSleepCommand.class, parseCommand(deleteSleepCommandString));
+    }
+
+    @Test
+    void parseCommand_listSleepCommand_expectListSleepCommand() throws AthletiException {
+        final String listSleepCommandString = "list-sleep";
+        assertInstanceOf(ListSleepCommand.class, parseCommand(listSleepCommandString));
     }
 }
