46edcdbfd294c5013e0757a607889e71809482be
DaDevChia <88506363+DaDevChia@users.noreply.github.com>
2023-10-14 14:29:33 +0800

    Added Junit test for sleep class

---
+++ src/test/java/athleticli/ui/sleep/SleepTest.java
@@ -0,0 +1,20 @@
+package athleticli.ui.sleep;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import athleticli.data.sleep.Sleep;
+
+
+
+public class SleepTest {
+    @Test
+    public void testSleepToString() {
+        Sleep sleep = new Sleep("10:00 PM", "6:00 AM");
+        assertEquals("sleep from 10:00 PM to 6:00 AM", sleep.toString());
+    }
+
+    
+
+}
