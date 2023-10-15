a5b6a6729ae36ee4aca74da6255acd9e24a33126
nihalzp <81457724+nihalzp@users.noreply.github.com>
2023-10-15 19:08:20 +0800

    Add DeleteDietCommand

---
+++ src/main/java/athleticli/commands/diet/DeleteDietCommand.java
@@ -0,0 +1,44 @@
+package athleticli.commands.diet;
+
+import athleticli.commands.Command;
+import athleticli.data.Data;
+import athleticli.data.diet.Diet;
+import athleticli.data.diet.DietList;
+import athleticli.exceptions.AthletiException;
+import athleticli.ui.Message;
+
+/**
+ * Executes the add diet commands provided by the user.
+ */
+public class DeleteDietCommand extends Command {
+    private final int index;
+
+    /**
+     * Constructor for AddDietCommand.
+     *
+     * @param diet Diet to be added.
+     */
+    public DeleteDietCommand(int index) {
+        this.index = index;
+    }
+
+    /**
+     * Updates the diet list.
+     *
+     * @param data The current data containing the diet list.
+     * @return The message which will be shown to the user.
+     */
+    public String[] execute(Data data) throws AthletiException {
+        if (index > data.getDiets().size() || index < 1) {
+            throw new AthletiException(Message.MESSAGE_INVALID_DIET_INDEX);
+        }
+        DietList dietList = data.getDiets();
+        int size = dietList.size();
+        Diet oldDiet = dietList.get(index - 1);
+        dietList.remove(index - 1);
+        return new String[]{Message.MESSAGE_DIET_DELETED, oldDiet.toString(),
+                String.format(Message.MESSAGE_DIET_COUNT, size - 1)};
+    }
+}
+  
+  
