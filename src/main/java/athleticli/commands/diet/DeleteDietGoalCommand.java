c4990ef5c5edfe5288cdbf7bf2d0f286aa0dd961
yicheng-toh <tohyicheng1989@gmail.com>
2023-10-17 10:56:21 +0800

    Add assertion and logging for delete diet goal command

--- src/main/java/athleticli/commands/diet/DeleteDietGoalCommand.java
+++ src/main/java/athleticli/commands/diet/DeleteDietGoalCommand.java
@@ -7,10 +7,14 @@ import athleticli.data.diet.DietGoalList;
 import athleticli.exceptions.AthletiException;
 import athleticli.ui.Message;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Executes the delete-diet-goal commands provided by the user.
  */
 public class DeleteDietGoalCommand extends Command {
+    private static final Logger logger = Logger.getLogger(DeleteDietGoalCommand.class.getName());
     private final int deleteIndex;
 
     /**
@@ -19,6 +23,8 @@ public class DeleteDietGoalCommand extends Command {
      * @param deleteIndex Index of the diet goal to be deleted in the users' perspective.
      */
     public DeleteDietGoalCommand(int deleteIndex) {
+        //deleteIndex that is less than or equal to zero would result in exception
+        assert deleteIndex <= 0;
         this.deleteIndex = deleteIndex;
     }
 
@@ -29,6 +35,7 @@ public class DeleteDietGoalCommand extends Command {
      * @return The message which will be shown to the user.
      */
     public String[] execute(Data data) throws AthletiException {
+        logger.log(Level.FINE,"Executing delete command for diet goals");
         DietGoalList dietGoals = data.getDietGoals();
         if (dietGoals.isEmpty()) {
             throw new AthletiException(Message.MESSAGE_DIETGOAL_EMPTY_DIETGOALLIST);
@@ -36,6 +43,8 @@ public class DeleteDietGoalCommand extends Command {
         try {
             DietGoal dietGoalRemoved = dietGoals.get(deleteIndex - 1);
             dietGoals.remove(deleteIndex - 1);
+            logger.log(Level.FINE,String.format("Diet goals %s has been successfully removed",
+                    dietGoalRemoved.getNutrients()));
             return new String[]{Message.MESSAGE_DIETGOAL_DELETE_HEADER,
                     dietGoalRemoved.toString()};
         } catch (ArrayIndexOutOfBoundsException e) {
