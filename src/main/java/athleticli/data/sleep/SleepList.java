1fda0f371511e7763971a8e38c52cc6ee6006686
DaDevChia <88506363+DaDevChia@users.noreply.github.com>
2023-10-20 01:12:14 +0800

    Deleted redundant sleeplist method

--- src/main/java/athleticli/data/sleep/SleepList.java
+++ src/main/java/athleticli/data/sleep/SleepList.java
@@ -6,16 +6,4 @@ import java.util.ArrayList;
  * Represents a list of sleep records.
  */
 public class SleepList extends ArrayList<Sleep> {
-    /**
-     * toString method for SleepList.
-     *
-     * @return String representation of the sleep list.
-     */
-    public String toString() {
-        StringBuilder output = new StringBuilder();
-        for (int i = 0; i < this.size(); i++) {
-            output.append((i + 1) + ". " + this.get(i).toString() + "\n");
-        }
-        return output.toString();
-    }
 }
