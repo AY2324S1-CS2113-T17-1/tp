11e07f26742dad96e7ab2d5401e90d6c36250ef5
Yang Ming-Tian <1178715749@qq.com>
2023-10-22 13:00:22 +0800

    Modify the extension of `PATH_SAVE`

--- src/main/java/athleticli/storage/Config.java
+++ src/main/java/athleticli/storage/Config.java
@@ -4,5 +4,5 @@ package athleticli.storage;
  * Defines string literals or configurations used for file storage.
  */
 public class Config {
-    public static final String PATH_SAVE = "./data/athleticli.txt";
+    public static final String PATH_SAVE = "./data/athleticli.bin";
 }
