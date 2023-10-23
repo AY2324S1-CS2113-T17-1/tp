2195ac4fd6c71f4ecfe27489c2b0f22eb2b84e44
AlWo223 <alexander.wolters@tum.de>
2023-10-23 21:09:14 +0800

    Enable autodelete of data file for text-ui-testing

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -8,6 +8,11 @@ cd ..
 
 cd text-ui-test
 
+if [ -e "data/athleticli.bin" ]
+then
+    rm data/athleticli.bin
+fi
+
 java  -jar $(find ../build/libs/ -mindepth 1 -print -quit) < input.txt > ACTUAL.TXT
 
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
