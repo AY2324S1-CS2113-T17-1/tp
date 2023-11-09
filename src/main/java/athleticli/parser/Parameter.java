1c7c49fcdd0d3a09676a04240c0d792c1d48e6f8
DaDevChia <88506363+DaDevChia@users.noreply.github.com>
2023-11-09 12:49:14 +0800

    Formatted Parameters and added index

--- src/main/java/athleticli/parser/Parameter.java
+++ src/main/java/athleticli/parser/Parameter.java
@@ -2,6 +2,17 @@ package athleticli.parser;
 
 
 public class Parameter {
+    /*  For Sleep and Activity */
+    public static final String START_TIME_SEPARATOR = "start/";
+    public static final String END_TIME_SEPARATOR = "end/";
+    public static final String INDEX_SEPARATOR = "index/";
+
+    /* For Acitivity */
+    public static final String SPORT_SEPARATOR = "sport/";
+    public static final String TYPE_SEPARATOR = "type/";
+    public static final String PERIOD_SEPARATOR = "period/";
+    public static final String TARGET_SEPARATOR = "target/";
+
     public static final String DURATION_SEPARATOR = "duration/";
     public static final String CAPTION_SEPARATOR = "caption/";
     public static final String DISTANCE_SEPARATOR = "distance/";
@@ -14,17 +25,11 @@ public class Parameter {
     public static final String SWIM_STORAGE_INDICATOR = "[Swim]:";
     public static final String DETAIL_FLAG = "-d";
 
+    /* For Diet */
     public static final String CALORIES_SEPARATOR = "calories/";
     public static final String PROTEIN_SEPARATOR = "protein/";
     public static final String CARB_SEPARATOR = "carb/";
     public static final String FAT_SEPARATOR = "fat/";
-
-    public static final String START_TIME_SEPARATOR = "start/";
-    public static final String END_TIME_SEPARATOR = "end/";
-    public static final String SPORT_SEPARATOR = "sport/";
-    public static final String TYPE_SEPARATOR = "type/";
-    public static final String PERIOD_SEPARATOR = "period/";
-    public static final String TARGET_SEPARATOR = "target/";
 
     public static final String NUTRIENTS_CALORIES = "calories";
     public static final String NUTRIENTS_PROTEIN = "protein";
