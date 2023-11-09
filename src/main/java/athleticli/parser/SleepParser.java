cdbaf6a086d93388cdfd40f89112afd2049ee003
DaDevChia <88506363+DaDevChia@users.noreply.github.com>
2023-11-09 12:49:45 +0800

    Standardized SleepParser

--- src/main/java/athleticli/parser/SleepParser.java
+++ src/main/java/athleticli/parser/SleepParser.java
@@ -3,9 +3,6 @@ package athleticli.parser;
 import java.time.LocalDateTime;
 
 import athleticli.data.Goal;
-import athleticli.commands.sleep.AddSleepCommand;
-import athleticli.commands.sleep.DeleteSleepCommand;
-import athleticli.commands.sleep.EditSleepCommand;
 import athleticli.data.sleep.Sleep;
 import athleticli.data.sleep.SleepGoal;
 import athleticli.exceptions.AthletiException;
@@ -13,6 +10,7 @@ import athleticli.ui.Message;
 
 public class SleepParser {
     //@@author  DaDevChia
+    /* Sleep Management */
     /**
      * Parses the raw user input for an add sleep command and returns the corresponding command object.
      *
@@ -20,108 +18,69 @@ public class SleepParser {
      * @return An object representing the slee0 add command.
      * @throws AthletiException
      */
-    public static AddSleepCommand parseSleepAdd(String commandArgs) throws AthletiException {
+    public static Sleep parseSleep(String commandArgs) throws AthletiException {
+        final int startDatetimeIndex = commandArgs.indexOf(Parameter.START_TIME_SEPARATOR);
+        final int endDatetimeIndex = commandArgs.indexOf(Parameter.END_TIME_SEPARATOR);
 
-        final int startTimeIndex = commandArgs.indexOf(Parameter.START_TIME_SEPARATOR);
-        final int endTimeIndex = commandArgs.indexOf(Parameter.END_TIME_SEPARATOR);
-
-        if (startTimeIndex == -1 || endTimeIndex == -1 || startTimeIndex > endTimeIndex) {
+        if (startDatetimeIndex == -1 || endDatetimeIndex == -1) {
             throw new AthletiException(Message.ERRORMESSAGE_PARSER_SLEEP_NO_START_END_DATETIME);
         }
 
-        final String startTimeStr =
-                commandArgs.substring(startTimeIndex + Parameter.START_TIME_SEPARATOR.length(), endTimeIndex)
+        final String startDatetimeStr =
+                commandArgs.substring(startDatetimeIndex + Parameter.START_TIME_SEPARATOR.length(), endDatetimeIndex)
                         .trim();
-        final String endTimeStr =
-                commandArgs.substring(endTimeIndex + Parameter.END_TIME_SEPARATOR.length()).trim();
+        final String endDatetimeStr =
+                commandArgs.substring(endDatetimeIndex + Parameter.END_TIME_SEPARATOR.length()).trim();
 
-        if (startTimeStr.isEmpty() || endTimeStr.isEmpty()) {
+        if (startDatetimeStr == null || startDatetimeStr.isEmpty() || endDatetimeStr == null || endDatetimeStr.isEmpty()) {
             throw new AthletiException(Message.ERRORMESSAGE_PARSER_SLEEP_NO_START_END_DATETIME);
         }
 
-        // Convert the strings to LocalDateTime
-        final LocalDateTime startTime = Parser.parseDateTime(startTimeStr);
-        final LocalDateTime endTime = Parser.parseDateTime(endTimeStr);
+        final LocalDateTime startDatetime = Parser.parseDateTime(startDatetimeStr);
+        final LocalDateTime endDatetime = Parser.parseDateTime(endDatetimeStr);
 
-        //Check if the start time is before the end time
-        if (startTime.isAfter(endTime)) {
+        if (startDatetime == null || endDatetime == null) {
+            throw new AthletiException(Message.ERRORMESSAGE_PARSER_SLEEP_INVALID_DATETIME);
+        }
+
+        if (startDatetime.isEqual(endDatetime)) {
+            throw new AthletiException(Message.ERRORMESSAGE_PARSER_SLEEP_START_END_SAME);
+        }
+
+        if (startDatetime.isAfter(endDatetime)) {
             throw new AthletiException(Message.ERRORMESSAGE_PARSER_SLEEP_END_BEFORE_START);
         }
 
-        return new AddSleepCommand(startTime, endTime);
+        return new Sleep(startDatetime, endDatetime);
     }
 
-    /**
-     * Parses the raw user input for a delete sleep command and returns the corresponding command object.
-     *
-     * @param commandArgs The raw user input containing the arguments.
-     * @return An object representing the sleep delete command.
-     * @throws AthletiException
-     */
-    public static DeleteSleepCommand parseSleepDelete(String commandArgs) throws AthletiException {
+    public static int parseSleepIndex(String commandArgs) throws AthletiException {
+        final int indexSeparatorIndex = commandArgs.indexOf(Parameter.INDEX_SEPARATOR);
+        if (indexSeparatorIndex == -1) {
+            throw new AthletiException(Message.ERRORMESSAGE_PARSER_SLEEP_NO_INDEX);
+        }
+        final String indexStr = commandArgs.substring(indexSeparatorIndex + Parameter.INDEX_SEPARATOR.length()).trim();
+        if (indexStr == null || indexStr.isEmpty()) {
+            throw new AthletiException(Message.ERRORMESSAGE_PARSER_SLEEP_NO_INDEX);
+        }
         int index;
-
         try {
-            index = Integer.parseInt(commandArgs.trim());
+            index = Integer.parseInt(indexStr);
         } catch (NumberFormatException e) {
-            throw new AthletiException(Message.ERRORMESSAGE_PARSER_SLEEP_DELETE_NO_INDEX);
+            throw new AthletiException(Message.ERRORMESSAGE_PARSER_SLEEP_INVALID_INDEX);
         }
-
-        return new DeleteSleepCommand(index);
+        return index;
     }
 
-    /**
-     * Parses the raw user input for an edit sleep command and returns the corresponding command object.
-     *
-     * @param commandArgs The raw user input containing the arguments.
-     * @return An object representing the sleep edit command.
-     * @throws AthletiException
-     */
-    public static EditSleepCommand parseSleepEdit(String commandArgs) throws AthletiException {
-        final int startTimeIndex = commandArgs.indexOf(Parameter.START_TIME_SEPARATOR);
-        final int endTimeIndex = commandArgs.indexOf(Parameter.END_TIME_SEPARATOR);
-        int index;
-
-        if (startTimeIndex == -1 || endTimeIndex == -1 || startTimeIndex > endTimeIndex) {
-            throw new AthletiException(Message.ERRORMESSAGE_PARSER_SLEEP_NO_START_END_DATETIME);
-        }
-
-        try {
-            index = Integer.parseInt(commandArgs.substring(0, startTimeIndex).trim());
-        } catch (NumberFormatException e) {
-            throw new AthletiException(Message.ERRORMESSAGE_PARSER_SLEEP_EDIT_NO_INDEX);
-        }
-
-        String startTimeStr =
-                commandArgs.substring(startTimeIndex + Parameter.START_TIME_SEPARATOR.length(), endTimeIndex)
-                        .trim();
-        String endTimeStr =
-                commandArgs.substring(endTimeIndex + Parameter.END_TIME_SEPARATOR.length()).trim();
-
-        if (startTimeStr.isEmpty() || endTimeStr.isEmpty()) {
-            throw new AthletiException(Message.ERRORMESSAGE_PARSER_SLEEP_NO_START_END_DATETIME);
-        }
-
-        // Convert the strings to LocalDateTime
-        LocalDateTime startTime;
-        LocalDateTime endTime;
-        startTime = Parser.parseDateTime(startTimeStr);
-        endTime = Parser.parseDateTime(endTimeStr);
-
-        //Check if the start time is before the end time
-        if (startTime.isAfter(endTime)) {
-            throw new AthletiException(Message.ERRORMESSAGE_PARSER_SLEEP_END_BEFORE_START);
-        }
-
-        return new EditSleepCommand(index, startTime, endTime);
-    }
-
+    /*  Sleep Goal Management */
     public static SleepGoal parseSleepGoal(String commandArgs) throws AthletiException {
         final int goalTypeIndex = commandArgs.indexOf(Parameter.TYPE_SEPARATOR);
         final int periodIndex = commandArgs.indexOf(Parameter.PERIOD_SEPARATOR);
         final int targetValueIndex = commandArgs.indexOf(Parameter.TARGET_SEPARATOR);
 
-        checkMissingSleepGoalParameters(goalTypeIndex, periodIndex, targetValueIndex);
+        if (goalTypeIndex == -1 || periodIndex == -1 || targetValueIndex == -1) {
+            throw new AthletiException(Message.ERRORMESSAGE_PARSER_SLEEP_GOAL_MISSING_PARAMETERS);
+        }
 
         if (goalTypeIndex > periodIndex || periodIndex > targetValueIndex) {
             throw new AthletiException(Message.ERRORMESSAGE_PARSER_SLEEP_GOAL_INVALID_PARAMETERS);
@@ -138,19 +97,6 @@ public class SleepParser {
         final int targetParsed = parseTarget(target);
 
         return new SleepGoal(goalType, timeSpan, targetParsed);
-    }
-
-    private static void checkMissingSleepGoalParameters(int goalTypeIndex, int periodIndex, int targetValueIndex)
-            throws AthletiException {
-        if (goalTypeIndex == -1 || periodIndex == -1 || targetValueIndex == -1) {
-            throw new AthletiException(Message.ERRORMESSAGE_PARSER_SLEEP_GOAL_MISSING_PARAMETERS);
-        }
-    }
-
-    private static void checkMissingSleepParameters(int startTimeIndex, int endTimeIndex) throws AthletiException {
-        if (startTimeIndex == -1 || endTimeIndex == -1) {
-            throw new AthletiException(Message.ERRORMESSAGE_PARSER_SLEEP_MISSING_PARAMETERS);
-        }
     }
 
     private static SleepGoal.GoalType parseGoalType(String type) throws AthletiException {
@@ -181,26 +127,9 @@ public class SleepParser {
         } catch (NumberFormatException e) {
             throw new AthletiException(Message.ERRORMESSAGE_PARSER_SLEEP_GOAL_INVALID_TARGET);
         }
-        if (targetParsed < 0) {
+        if (targetParsed <= 0) {
             throw new AthletiException(Message.ERRORMESSAGE_PARSER_SLEEP_GOAL_INVALID_TARGET);
         }
         return targetParsed;
     }
-
-    public static Sleep parseSleep(String s) throws AthletiException {
-        final int startTimeIndex = s.indexOf(Parameter.START_TIME_SEPARATOR);
-        final int endTimeIndex = s.indexOf(Parameter.END_TIME_SEPARATOR);
-
-        checkMissingSleepParameters(startTimeIndex, endTimeIndex);
-
-        final String startTimeStr =
-                s.substring(startTimeIndex + Parameter.START_TIME_SEPARATOR.length(), endTimeIndex).trim();
-        final String endTimeStr = s.substring(endTimeIndex + Parameter.END_TIME_SEPARATOR.length()).trim();
-
-        LocalDateTime startTime = Parser.parseDateTime(startTimeStr);
-        LocalDateTime endTime = Parser.parseDateTime(endTimeStr);
-
-        return new Sleep(startTime, endTime);
-    }
-    
 }
