527820c01c1542c45d08cd8723e8d7f69e363e2c
nihalzp <81457724+nihalzp@users.noreply.github.com>
2023-11-11 20:29:30 +0800

    Fix tests for DeleteActivityGoalCommandTest

--- src/test/java/athleticli/commands/activity/DeleteActivityGoalCommandTest.java
+++ src/test/java/athleticli/commands/activity/DeleteActivityGoalCommandTest.java
@@ -2,9 +2,13 @@ package athleticli.commands.activity;
 
 
 import athleticli.data.Data;
+import athleticli.data.Goal.TimeSpan;
 import athleticli.data.activity.ActivityGoal;
+import athleticli.data.activity.ActivityGoal.GoalType;
+import athleticli.data.activity.ActivityGoal.Sport;
 import athleticli.data.activity.ActivityGoalList;
 import athleticli.exceptions.AthletiException;
+import athleticli.ui.Message;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -14,12 +18,13 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class DeleteActivityGoalCommandTest {
 
+    ActivityGoalList activityGoals;
     private Data data;
 
     @BeforeEach
     void setUp() {
         data = new Data();
-        ActivityGoalList activityGoals = data.getActivityGoals();
+        activityGoals = data.getActivityGoals();
         ActivityGoal goal1 = new ActivityGoal(ActivityGoal.TimeSpan.WEEKLY, ActivityGoal.GoalType.DISTANCE,
                 ActivityGoal.Sport.RUNNING, 10);
         ActivityGoal goal2 = new ActivityGoal(ActivityGoal.TimeSpan.MONTHLY, ActivityGoal.GoalType.DURATION,
@@ -36,22 +41,18 @@ public class DeleteActivityGoalCommandTest {
 
     @Test
     void execute_existingActivityGoal_editsActivityGoal() throws athleticli.exceptions.AthletiException {
-        ActivityGoal goal = new ActivityGoal(athleticli.data.activity.ActivityGoal.TimeSpan.WEEKLY,
-                athleticli.data.activity.ActivityGoal.GoalType.DISTANCE,
-                athleticli.data.activity.ActivityGoal.Sport.RUNNING, 0);
-        EditActivityGoalCommand command = new EditActivityGoalCommand(goal);
+        ActivityGoal goal = new ActivityGoal(TimeSpan.WEEKLY, GoalType.DISTANCE, Sport.RUNNING, 0);
+        DeleteActivityGoalCommand command = new DeleteActivityGoalCommand(goal);
         String[] expected =
-                new String[]{athleticli.ui.Message.MESSAGE_ACTIVITY_GOAL_EDITED, goal.toString(data)};
+                new String[]{Message.MESSAGE_ACTIVITY_GOAL_DELETED, activityGoals.get(0).toString(data)};
         String[] actual = command.execute(data);
         assertArrayEquals(expected, actual);
     }
 
     @Test
     void execute_nonExistingActivityGoal_throwsAthletiException() {
-        ActivityGoal goal = new ActivityGoal(athleticli.data.activity.ActivityGoal.TimeSpan.YEARLY,
-                athleticli.data.activity.ActivityGoal.GoalType.DISTANCE,
-                athleticli.data.activity.ActivityGoal.Sport.RUNNING, 0);
-        EditActivityGoalCommand command = new EditActivityGoalCommand(goal);
+        ActivityGoal goal = new ActivityGoal(TimeSpan.YEARLY, GoalType.DISTANCE, Sport.RUNNING, 0);
+        DeleteActivityGoalCommand command = new DeleteActivityGoalCommand(goal);
         assertThrows(AthletiException.class, () -> command.execute(data));
     }
 }
